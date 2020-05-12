8bbe1c419e740458277db16636a07fe959c9bb6b
Reiko Makimoto <gracesystems2016@gmail.com>
2020-05-12 20:12:18 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -10,7 +10,21 @@ function fibonacci(n) {
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
+
+function fibonacciMap(n) {
+  if (memo.has(n)) {
+    return memo.get(n);
+  }
+  const value = fibonacciMap(n - 1) + fibonacciMap(n - 2);
+  memo.set(n, value);
+  return value;
+}
+
 const lenght = 40;
 for (let i = 0; i <= lenght; i++) {
-  console.log(fibonacci(i));
+  // console.log(fibonacci(i));
+  console.log(fibonacciMap(i));
 }
